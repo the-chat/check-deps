aedbd6b138132591ffaef272a9d9f5a32b5eb23e
the-chat <thechatllc@gmail.com>
2022-04-13 21:04:59 +0500

    files order

--- src/get/files.ts
+++ src/get/files.ts
@@ -1,10 +1,10 @@
 export type Files = string[]
 
 export const files: Files = [
-  "../pkgs/*",
   "../the-chat-sso",
   "../the-chat",
   "../the-chat-note",
   "../the-chat-info",
   "../test-ui-kit",
+  "../pkgs/*",
 ]
